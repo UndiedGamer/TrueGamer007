769f7227869932835334f079eebf515b2ece77fa
UndiedGamer <hemanthchakka2007@gmail.com>
2021-08-10 20:50:49 +0530

    Hoping this to work

--- slash/vote.js
+++ slash/vote.js
@@ -3,6 +3,6 @@ const { actionRow } = require("../constants/voteconst")
 module.exports = {
 	name: 'vote',
 	execute: async (interaction) => {
-		await interaction.reply({ content: `Hello ${interaction.user}! please select something from below`, components: [actionRow] })
+		await interaction.reply({ content: `Hello ${interaction.user}! please select something from below`, components: [actionRow], ephermal: true })
 	}
 }
